e62a5533532d2b3c2c4dcd2ec31bbb047e055f21
sohailkatpar145 <sohailkatpar145@gmail.com>
2024-06-12 17:26:37 +0500

    display records functionality added

--- app/src/main/java/com/example/libraryapp/MainActivity.kt
+++ app/src/main/java/com/example/libraryapp/MainActivity.kt
@@ -6,13 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -21,6 +28,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import com.example.libraryapp.repository.Repository
 import com.example.libraryapp.room.BookEntity
@@ -76,6 +85,42 @@ fun MainScreen(viewModel: BookViewModel) {
             viewModel.addBook(BookEntity(0, inputBook))
         }) {
             Text(text = "Insert book in db")
+        }
+
+        BookList(viewModel = viewModel)
+    }
+}
+
+@Composable
+fun BookCard(viewModel: BookViewModel, book: BookEntity){
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Row() {
+            Text(
+                text = book.id.toString(),
+                fontSize = 24.sp,
+                modifier = Modifier.padding(start = 4.dp, end = 4.dp)
+            )
+            Text(
+                text = book.title,
+                fontSize = 24.sp,
+            )
+        }
+    }
+}
+
+
+@Composable
+fun BookList(viewModel: BookViewModel) {
+    val books by viewModel.books.collectAsState(initial = emptyList())
+    LazyColumn (){
+        items(items = books) {
+            item -> BookCard(
+            viewModel = viewModel,
+            book = item)
         }
     }
 }
