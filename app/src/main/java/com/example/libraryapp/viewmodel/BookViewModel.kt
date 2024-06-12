6eebe7e90751cb2a2588f8af9155381060d281e1
sohailkatpar145 <sohailkatpar145@gmail.com>
2024-06-12 16:08:14 +0500

    files updated

---
+++ app/src/main/java/com/example/libraryapp/viewmodel/BookViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.libraryapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.libraryapp.repository.Repository
+import com.example.libraryapp.room.BookEntity
+import kotlinx.coroutines.launch
+
+class BookViewModel (val repository: Repository): ViewModel(){
+
+    fun addBook(book: BookEntity){
+        viewModelScope.launch {
+            repository.addBookToRoom(book)
+        }
+    }
+}
